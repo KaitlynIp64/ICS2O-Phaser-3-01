f9494e02c7f4cee443df6eabbec9e0ab0355394c
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-15 19:46:45 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 // Copyright (c) 2022 Kaitlyn Ip All rights reserved
 //
 // Created by: Kaitlyn Ip
-// Created on: Mar 2022
-// This file contains xxx
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
 
-/**
- * This function xxx.
- */
+
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+};
+
+const game = new Phaser.Game(config);
+console.log(game);
