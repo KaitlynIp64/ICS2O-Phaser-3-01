7cac0cd80e7a2271428f459ef53faf7b31f77cc9
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-27 02:38:43 +0000

    added code

--- js/titleScene.js
+++ js/titleScene.js
@@ -1,20 +1,20 @@
-/*global Phaser */
+/* global Phaser */
 
 // Copyright (c) 2022 Kaitlyn Ip All rights reserved
 // 
 // Created by: Kaitlyn Ip
 // Created on: Apr 2022
-// This is the Splash Scene
+// This is the Title Scene
 
 /**
- * This class is the Splash Scene.
+ * This class is the Title Scene.
  */
-class SplashScene extends Phaser.Scene {
+class TitleScene extends Phaser.Scene {
   /**
    * This method is the constructor.
    */
   constructor () {
-    super({key: 'splashScene' })
+    super({key: 'titleScene' })
   }
 
   /**
@@ -32,7 +32,7 @@ class SplashScene extends Phaser.Scene {
  * Use it to load assets.
  */
   preload() {
-    console.log("Splash Scene")
+    console.log("Title Scene")
   }
 
 /**
@@ -51,8 +51,8 @@ class SplashScene extends Phaser.Scene {
  * @param {number} delta - The delta time in ms since the last frame. 
  */
   update(time, delta) {
-    this.scene.switch("titleScene")
+    // pass
   }
 }
 
-export default SplashScene
+export default TitleScene
