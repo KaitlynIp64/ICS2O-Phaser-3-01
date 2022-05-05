42ec8f27a4eebcfc16116211d11e2f766538556c
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-05 02:58:36 +0000

    added code

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,8 +7,12 @@
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
+
   constructor() {
     super({ key: 'gameScene' })
+
+    this.background = null
+    this.ship = null
   }
 
   init(data) {
@@ -17,9 +21,16 @@ class GameScene extends Phaser.Scene {
 
   preload() {
     console.log('Game Scene')
+
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
   }
 
   create(data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0,0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
   update(time, delta) {
