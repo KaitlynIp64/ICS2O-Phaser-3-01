67fff7634f7f18999feb4e328acccbc3c657b83c
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-26 17:22:44 +0000

    added code

--- js/gameScene.js
+++ js/gameScene.js
@@ -42,6 +42,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio('laser', 'assets/laser1.wav')
     this.load.audio('explosion', 'assets/barrelExploding.wav')
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
 
@@ -73,6 +74,18 @@ class GameScene extends Phaser.Scene {
         this.createAlien()
         this.createAlien()
       }.bind(this))
+
+    // Collisions between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ userHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+    }.bind(this))
+    
   }
 
   update(time, delta) {
